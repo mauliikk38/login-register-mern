3df439d61ee400daf96ba294e2624d07b6540c46
mauliikk38 <maulikkotadiya60@gmail.com>
2022-03-29 11:36:38 +0530

    clone

--- login-register-frontend/src/App.js
+++ login-register-frontend/src/App.js
@@ -3,32 +3,24 @@ import Homepage from "./components/homepage/homepage"
 import Login from "./components/login/login"
 import Register from "./components/register/register"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 function App() {
 
   const [ user, setLoginUser] = useState({})
 
-  useEffect(() => {
-    setLoginUser(JSON.parse(localStorage.getItem("MyUser")))
-  }, [])
-
-  const updateUser = (user) => {
-    localStorage.setItem("MyUser", JSON.stringify(user))
-    setLoginUser(user)
-  }
-
+  
   return (
     <div className="App">
       <Router>
         <Switch>
           <Route exact path="/">
             {
-              user && user._id ? <Homepage updateUser={updateUser} /> : <Login updateUser={updateUser}/>
+              user && user._id ? <Homepage setLoginUser={setLoginUser} /> : <Login setLoginUser={setLoginUser}/>
             }
           </Route>
           <Route path="/login">
-            <Login updateUser={updateUser}/>
+            <Login setLoginUser={setLoginUser}/>
           </Route>
           <Route path="/register">
             <Register />
--- login-register-frontend/src/components/homepage/homepage.js
+++ login-register-frontend/src/components/homepage/homepage.js
@@ -1,11 +1,11 @@
 import React from "react"
 import "./homepage.css"
 
-const Homepage = ({updateUser}) => {
+const Homepage = ({setLoginUser}) => {
     return (
         <div className="homepage">
             <h1>Hello Homepage</h1>
-            <div className="button" onClick={() => updateUser({})} >Logout</div>
+            <div className="button" onClick={() => setLoginUser({})} >Logout</div>
         </div>
     )
 }
